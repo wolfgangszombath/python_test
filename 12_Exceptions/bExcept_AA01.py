776408524880c1552d01c77496ef4b6d75b43b48
Wolfgang <w.szombath@szf.at>
2022-09-29 12:56:44 +0200

    echo "# python_test" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/wolfgangszombath/python_test.git git push -u origin main

---
+++ 12_Exceptions/bExcept_AA01.py
@@ -0,0 +1,11 @@
+import validate_email
+#email = input("Geben Sie Ihre e-Mail Adresse ein:")
+
+email = "abc@def.fr"
+
+
+try:
+    email = validate_email
+    print(str(email))
+except:
+    print("Error DU Gumpfi")
